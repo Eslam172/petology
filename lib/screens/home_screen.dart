4e1fe6fbdb1b324de97de3cb4984bea47cad5e5a
Eslam172 <eslamelfnanee@gmail.com>
2022-08-08 13:00:44 +0200

    adaption

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:petology/screens/Login_screen.dart';
 import 'package:petology/screens/about_us_screen.dart';
+import 'package:petology/screens/adaption_screen.dart';
 import 'package:petology/screens/request_screen.dart';
 import 'package:petology/screens/signup_screen.dart';
 import 'package:petology/shared/styles/colors.dart';
@@ -80,7 +81,7 @@ class HomeScreen extends StatelessWidget {
                     child: AboutUsScreen(),
                   ),
                   Center(
-                    child: Text('second page'),
+                    child: AdaptionScreen(),
                   ),
                   Center(
                     child: Text('third page'),
